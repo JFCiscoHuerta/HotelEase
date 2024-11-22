658f986cfc53614a2b7e2d02d7f048550e0dbd65
JFCiscoHuerta <151865620+JFCiscoHuerta@users.noreply.github.com>
2024-11-22 00:00:35 -0600

    test: add basic config to RoomControllerTest

--- Room/src/test/java/org/gklyphon/room/controller/RoomControllerTest.java
+++ Room/src/test/java/org/gklyphon/room/controller/RoomControllerTest.java
@@ -1,7 +1,31 @@
 package org.gklyphon.room.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.gklyphon.room.service.impl.RoomServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
+@AutoConfigureMockMvc
 class RoomControllerTest {
 
+    @MockBean
+    RoomServiceImpl roomService;
+
+    @Autowired
+    MockMvc mockMvc;
+
+    ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setUp() {
+        objectMapper.registerModule(new JavaTimeModule());
+    }
 }
