8535de7c7b32d6f13ee00b8adb665f6f2c0dc54c
JFCiscoHuerta <151865620+JFCiscoHuerta@users.noreply.github.com>
2024-11-23 00:20:06 -0600

    feat: add RoomImage and RoomFeature mapper methods

--- Room/src/main/java/org/gklyphon/room/mapper/IRoomMapper.java
+++ Room/src/main/java/org/gklyphon/room/mapper/IRoomMapper.java
@@ -1,15 +1,29 @@
 package org.gklyphon.room.mapper;
 
+import org.gklyphon.room.model.dtos.RoomFeatureRegisterDTO;
+import org.gklyphon.room.model.dtos.RoomImageRegisterDTO;
 import org.gklyphon.room.model.dtos.RoomRegisterDTO;
 import org.gklyphon.room.model.entities.Room;
+import org.gklyphon.room.model.entities.RoomFeature;
+import org.gklyphon.room.model.entities.RoomImage;
 import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface IRoomMapper {
 
     RoomRegisterDTO toRoomRegisterDTO(Room room);
+    List<RoomRegisterDTO> toRoomRegisterDTOS(Iterable<Room> rooms);
 
     @InheritInverseConfiguration
     Room toRoom(RoomRegisterDTO roomRegisterDTO);
+    List<Room> toRooms(Iterable<RoomRegisterDTO> roomRegisterDTOS);
+
+    RoomImage toRoomImage(RoomImageRegisterDTO roomImageRegisterDTO);
+    List<RoomImage> toRoomImages(Iterable<RoomImageRegisterDTO> roomImageRegisterDTOS);
+
+    RoomFeature toRoomFeature(RoomFeatureRegisterDTO roomFeatureRegisterDTO);
+    List<RoomFeature> toRoomFeatures(Iterable<RoomFeatureRegisterDTO> roomFeatureRegisterDTOS);
 }
