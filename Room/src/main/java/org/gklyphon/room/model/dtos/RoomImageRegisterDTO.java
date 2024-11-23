b03ddb6150b4e58e2aee55eaa6f4afae7ffcae89
JFCiscoHuerta <151865620+JFCiscoHuerta@users.noreply.github.com>
2024-11-23 00:25:38 -0600

    refactor: remove roomId property

--- Room/src/main/java/org/gklyphon/room/model/dtos/RoomImageRegisterDTO.java
+++ Room/src/main/java/org/gklyphon/room/model/dtos/RoomImageRegisterDTO.java
@@ -1,7 +1,6 @@
 package org.gklyphon.room.model.dtos;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @Builder
@@ -13,7 +12,4 @@ public class RoomImageRegisterDTO {
 
     @NotBlank
     private String path;
-
-    @NotNull
-    private Long roomId;
 }
