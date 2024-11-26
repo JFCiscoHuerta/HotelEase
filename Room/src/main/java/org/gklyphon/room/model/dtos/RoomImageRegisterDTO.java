38dd901e35d850cd011b630f4dadd654d0ebf5db
JFCiscoHuerta <151865620+JFCiscoHuerta@users.noreply.github.com>
2024-11-25 23:26:28 -0600

    docs: add RoomImageRegisterDTO class documentation

--- Room/src/main/java/org/gklyphon/room/model/dtos/RoomImageRegisterDTO.java
+++ Room/src/main/java/org/gklyphon/room/model/dtos/RoomImageRegisterDTO.java
@@ -3,6 +3,10 @@ package org.gklyphon.room.model.dtos;
 import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
+/**
+ * Data Transfer Object (DTO) for registering a new room image.
+ * This class is used to capture the path of the image associated with the room.
+ */
 @Builder
 @Getter
 @Setter
@@ -10,6 +14,10 @@ import lombok.*;
 @NoArgsConstructor
 public class RoomImageRegisterDTO {
 
+    /**
+     * The path where the image is stored.
+     * This field cannot be blank.
+     */
     @NotBlank
     private String path;
 }
