fe31ccf240896d9faf1b66495604b003a7c7293b
JFCiscoHuerta <151865620+JFCiscoHuerta@users.noreply.github.com>
2024-11-25 23:27:24 -0600

    docs: add RoomType class documentation

--- Room/src/main/java/org/gklyphon/room/model/entities/enums/RoomType.java
+++ Room/src/main/java/org/gklyphon/room/model/entities/enums/RoomType.java
@@ -1,7 +1,29 @@
 package org.gklyphon.room.model.entities.enums;
 
+/**
+ * Enum representing the different types of rooms available.
+ *
+ * <p>This enum defines the types of rooms, such as simple rooms, double rooms, and suites,
+ * which may vary in size and amenities.</p>
+ *
+ * @author JFCiscoHuerta
+ * @version 1.0
+ * @since 25-Nov-2024
+ */
 public enum RoomType {
+
+    /**
+     * A simple room, typically a small room with basic amenities.
+     */
     SIMPLE,
+
+    /**
+     * A double room, typically with two beds or a larger space suitable for two people.
+     */
     DOUBLE,
+
+    /**
+     * A suite room, typically larger with additional amenities such as a living area.
+     */
     SUITE
 }
