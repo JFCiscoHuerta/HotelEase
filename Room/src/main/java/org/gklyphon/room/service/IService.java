4c2b2469795489c9cc5117339cf5a2a786d8b235
JFCiscoHuerta <151865620+JFCiscoHuerta@users.noreply.github.com>
2024-11-25 23:24:10 -0600

    docs: add IService interface documentation

--- Room/src/main/java/org/gklyphon/room/service/IService.java
+++ Room/src/main/java/org/gklyphon/room/service/IService.java
@@ -3,8 +3,35 @@ package org.gklyphon.room.service;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+/**
+ * Generic service interface for basic CRUD operations.
+ * This interface provides common methods to perform CRUD operations
+ * on entities of type {@code T}.
+ *
+ * @param <T> the type of the entity that the service manages
+ */
 public interface IService <T> {
+
+    /**
+     * Finds an entity by its ID.
+     *
+     * @param id the ID of the entity to be found
+     * @return the entity of type {@code T} if found, otherwise {@code null}
+     */
     public T findById(Long id);
+
+    /**
+     * Retrieves a paginated list of all entities of type {@code T}.
+     *
+     * @param pageable pagination details (page number, size, etc.)
+     * @return a {@link Page} of entities of type {@code T}
+     */
     public Page<T> findAll(Pageable pageable);
+
+    /**
+     * Deletes an entity by its ID.
+     *
+     * @param id the ID of the entity to be deleted
+     */
     public void delete(Long id);
 }
