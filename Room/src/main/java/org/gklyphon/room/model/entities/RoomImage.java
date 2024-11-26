d84f85d0ad6ddb62f7246a9237b874efd196b44d
JFCiscoHuerta <151865620+JFCiscoHuerta@users.noreply.github.com>
2024-11-25 23:26:09 -0600

    docs: add RoomImage class documentation

--- Room/src/main/java/org/gklyphon/room/model/entities/RoomImage.java
+++ Room/src/main/java/org/gklyphon/room/model/entities/RoomImage.java
@@ -5,6 +5,15 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
+/**
+ * Represents an image associated with a room.
+ *
+ * <p>This class extends {@link Auditable} to include audit fields for creation and update timestamps.</p>
+ *
+ * @author JFCiscoHuerta
+ * @version 1.0
+ * @since 25-Nov-2024
+ */
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
@@ -14,13 +23,23 @@ import lombok.*;
 @Table(name = "images")
 public class RoomImage extends Auditable {
 
+    /**
+     * The unique identifier for the image.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    /**
+     * The path or URL of the image. Cannot be blank.
+     */
     @NotBlank
     private String path;
 
+    /**
+     * The room to which the image is associated.
+     * This field is ignored during JSON serialization.
+     */
     @ManyToOne
     @JsonIgnore
     private Room room;
