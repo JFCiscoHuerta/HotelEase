5f2070e6b6cd801fed2ad5d174347b1bec1a51fc
JFCiscoHuerta <151865620+JFCiscoHuerta@users.noreply.github.com>
2024-12-02 23:59:23 -0600

    docs: add RoomReservation javadoc

--- Reservation/src/main/java/org/gklyphon/Reservation/models/entities/RoomReservation.java
+++ Reservation/src/main/java/org/gklyphon/Reservation/models/entities/RoomReservation.java
@@ -6,6 +6,17 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * Entity representing a room reservation. This class extends {@link Auditable}
+ * to inherit audit fields for tracking creation and update timestamps.
+ * <p>
+ * This entity links a specific room to a reservation, storing the room's unique identifier.
+ * </p>
+ *
+ * @author JFCiscoHuerta
+ * @version 1.0
+ * @since 2-Dec-2024
+ */
 @Builder
 @Getter
 @Setter
@@ -14,10 +25,16 @@ import lombok.Setter;
 @Table(name = "room_reservations")
 public class RoomReservation extends Auditable {
 
+    /**
+     * Unique identifier for the room reservation.
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    /**
+     * The unique identifier of the room associated with this reservation.
+     */
     private Long roomId;
 
 }
